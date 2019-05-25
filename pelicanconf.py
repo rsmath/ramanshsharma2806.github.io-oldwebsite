209b5974b4e98faa429f2a19cd4c5d33d646bf84
Ramansh Sharma <sharmar@bxscience.edu>
2019-05-25 22:52:24 +0530

    i forgot which commit is this

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,25 +13,23 @@ TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
-RSS_FEED_SUMMARY_ONLY = False
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Bronx Science', 'https://bxscience.edu'),
+LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('Twitter', 'https://twitter.com/ramanshsharma1'),
-          ('GitHub', 'https://github.com/ramanshsharma2806'),
-          ('Linkedin', 'https://www.linkedin.com/in/ramanshsharma/'),)
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
-# Images
-STATIC_PATHS = ['images']
-
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = False
+#RELATIVE_URLS = True
--- publishconf.py
+++ publishconf.py
@@ -21,5 +21,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "ramanshsharma2806"
+#DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
