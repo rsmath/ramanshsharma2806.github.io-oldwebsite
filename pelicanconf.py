38d2ed2aa66c936380b2fd689ba05101f9a0698f
Ramansh Sharma <sharmar@bxscience.edu>
2019-05-25 23:00:56 +0530

    sixth maybe commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,23 +13,25 @@ TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
+LINKS = (('Bronx Science', 'https://bxscience.edu'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/ramanshsharma1'),
+          ('GitHub', 'https://github.com/ramanshsharma2806'),
+          ('Linkedin', 'https://www.linkedin.com/in/ramanshsharma/'),)
 
 DEFAULT_PAGINATION = 10
+
+# Images
+STATIC_PATHS = ['images']
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = False
